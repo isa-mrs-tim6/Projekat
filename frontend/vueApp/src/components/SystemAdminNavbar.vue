880e97afa489b70c416acac5c66e6f2b6ab3688d
Nemanja Janković <nemanja997@gmail.com>
2019-03-27 09:53:35 +0100

    Dodavanje .vue fajlova za system admina, nedovrseno dodavanje hotela

---
+++ frontend/vueApp/src/components/SystemAdminNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Manage admin accounts</v-btn>
+            <v-btn flat>Airlines</v-btn>
+            <v-btn flat>Hotels</v-btn>
+            <v-btn flat>Rent-a-car companies</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "SystemAdminNavbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/vueApp/src/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//import Home from './views/Home.vue'
+import SystemAdmin from "./views/SystemAdmin";
+import HotelAdmin from "./views/HotelAdmin";
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: SystemAdmin
+    },
+    {
+      path: '/systemAdmin',
+      component: SystemAdmin
+    },
+    {
+      path: '/hotelAdmin',
+      component: HotelAdmin
+    }
+  ]
+})
